1a1ac907d2c1924721f0e5c395333bee6e53f7f9
Chance Snow <git@chancesnow.me>
2020-04-08 21:18:27 -0700

    Add placeholder Zsh theme

---
+++ home/.oh-my-zsh/custom/themes/chances.theme.zsh
@@ -0,0 +1,11 @@
+# TODO: A prompt that shows the machine name on SSH connections
+
+# Reference:
+# https://github.com/ohmyzsh/ohmyzsh/wiki/Themes#blinks
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/themes/blinks.zsh-theme
+
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export PS1=''
+# else
+#   export PS1=''
+# fi
